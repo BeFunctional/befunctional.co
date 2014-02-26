9b85e19e9e0413d9e4d26f177ae4d567516f67aa
Coty Beasley <design@beasleycreative.com>
2014-02-26 17:32:57 -0600

    Got modal to fire and gular to pull input data in. Still need to hook up mailchimp though.

--- js/main.js
+++ js/main.js
@@ -5,4 +5,59 @@
  *  This controls inflatable text.
  * -------------------------------------------
  */
+
 $('.fitText').fitText(1); // Turn the compressor down (resizes less aggressively)
+
+
+
+
+
+
+/*
+ * -------------------------------------------
+ *	$_EMAIL VALIDATION
+ * -------------------------------------------
+ */
+
+function validateEmail($email) {
+  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  if( !emailReg.test( $email ) ) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+var emailValID = "#userEmail";
+$("label[for='mce-EMAIL'], input[name='EMAIL'], .invalidEmail").hide();
+$(emailValID).keydown(function(event){
+  if(event.keyCode == 13){
+    $('.newsletterTrigger').click();
+  }
+});
+
+$('#newsletterSignupModal').on('shown.bs.modal', function (e) {
+	console.log('Jesus Christ, about fucking time.');
+	$(emailValID).val('');
+	$(emailValID).parent('.input-group').removeClass('has-warning has-feedback');
+	$('.invalidEmail').slideUp().fadeOut();
+});
+
+$(".newsletterSignup").find(".input-group-btn .btn").click( function() {
+    var emailVal = $(emailValID).val();
+    if (emailVal != "" && validateEmail(emailVal)) {
+        $('#newsletterSignupModal').modal('show');
+    } else {
+        $(emailValID).parent('.input-group').addClass('has-warning has-feedback');
+        $('.invalidEmail').finish().slideDown().fadeIn();
+    }
+});
+$("#newsletterSignupModal").find("#mc-embedded-subscribe").click( function() {
+  if (!$('#newsletterSignupModal input').val() === '') {
+    alert('success!');
+  } else {
+    alert('Fail!');
+  }
+});
+
+
