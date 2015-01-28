311fdfc6cbcc73c2f0ec491c1e1e7f1af70118f9
Coty Beasley <coty@cotybeasley.name>
2015-01-27 20:59:26 -0600

    Refined the layout.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,20 +12,14 @@ var app = angular.module('functionalApp', ['ngMaterial']);
 /* ==========================================================================
  $_CONTROLLERS --------------------------------------------------------------
  ========================================================================== */
-app.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log) {
-  $scope.toggleLeft = function() {
-    $mdSidenav('left').toggle()
-      .then(function(){
-        $log.debug("toggle left is done");
-      });
+app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+  $scope.toggleSidenav = function(menuId) {
+    $mdSidenav(menuId).toggle();
   };
-});
-
-app.controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
   $scope.close = function() {
     $mdSidenav('left').close()
       .then(function(){
         $log.debug("close LEFT is done");
       });
   };
-});
+}]);
